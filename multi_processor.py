14b0714c44f69359427cc07f97f7d29239a6121a
James Hatfield <jahatfi@g.clemson.edu>
2022-03-01 09:16:18 -0500

    new branch

--- multi_processor.py
+++ multi_processor.py
@@ -1,20 +1,37 @@
 import os
 import pprint
 import re
-import shutil
 import sys
 import time
 from pptx import Presentation
 from utils import is_directory, is_filename
 from PyPDF2 import PdfFileReader
 import fitz
+import tempfile
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
 
-# TODO Update
+# If you don't have tesseract executable in your PATH, include the following:
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
+
+# TODO Update with mandatory sections, then check for their presence
 sections = [
     "Method",
     "Approach"
 ]
 
+key_phrases = [
+    "DAF CUSTOMER",
+    "DAF End-User",
+    "Digitally signed by",
+    "TPOC"
+]
+
+key_regexes = [
+    "TPOC[Ss]?\)?:"
+]
+# ==============================================================================
 def get_total_budget(file_name,
                     max_value = 1250000,
                     keyphrase="Total Dollar Amount for this Proposal"):
@@ -35,25 +52,25 @@ def get_total_budget(file_name,
                         print(f"WARNING!  Proposed budget exceeds threshold of ${threshold}!")
                     break
             #print(text)    
-
-def process_pdf_sigs(file_name):
+# ==============================================================================
+def process_pdf_sigs_fitz(file_name):
     """
     Print info about digital signatures, as well as text and surrounding 
     text containing any of the key phrases defined below
     """
     text = ''
-    key_phrases = [
-        "DAF CUSTOMER",
-        "DAF End-User",
-        "Digitally signed by"
-    ]
-    key_regexes = [
-        "TPOC[Ss]?\)?:"
-    ]
-    with fitz.open(file_name ) as doc:
+
+
+    got_text = False
+    with fitz.open(file_name) as doc:
         # Iterate over every page in the doc
         for page_count, page in enumerate(doc):
             text_segs = page.getText().split('\n')
+            text_segs = [text for text in text_segs if text]
+            if not text_segs:
+                continue
+            got_text = True
+            print(text_segs)   
             # Iterate over every text field
             for seg_i in range(len(text_segs)):
                 single_text = text_segs[seg_i]
@@ -71,6 +88,109 @@ def process_pdf_sigs(file_name):
                         seg_i += 5
                         break
 
+    if not got_text:
+        print("Failed to get text with fitz parser")
+        
+    return got_text
+
+# ==============================================================================
+def ocr_cleanup(open_file_handle, open_file_name, files_to_remove):
+    print("REmoving")
+    for file_name_to_remove in files_to_remove:
+        os.remove(file_name_to_remove)
+    if open_file_handle:
+        open_file_handle.close()
+    if open_file_name:
+        os.remove(open_file_name)
+
+# ==============================================================================
+def ocr_pdf(file_name):
+    """
+    https://www.geeksforgeeks.org/python-reading-contents-of-pdf-using-ocr-optical-character-recognition/
+    """
+    print(f"OCR'ing {file_name}.  This could take a minute.")
+    # Counter to store images of each page of PDF to image
+    image_counter = 1
+    files_to_remove = []
+    pages = convert_from_path(file_name, 500)
+    # Iterate through all the pages stored above
+    for page in pages:
+  
+        # Declaring filename for each page of PDF as JPG
+        # For each page, filename will be:
+        # PDF page 1 -> page_1.jpg
+        # PDF page 2 -> page_2.jpg
+        # PDF page 3 -> page_3.jpg
+        # ....
+        # PDF page n -> page_n.jpg
+        filename = "page_"+str(image_counter)+".jpg"
+        
+        
+        # Save the image of the page in system
+        page.save(filename, 'JPEG')
+        files_to_remove.append(filename)
+    
+        # Increment the counter to update filename
+        image_counter = image_counter + 1
+    
+        '''
+        Part #2 - Recognizing text from the images using OCR
+        '''
+            
+        # Variable to get count of total number of pages
+        filelimit = image_counter-1
+        
+        # Creating a text file to write the output
+        outfile = "out_text.txt"
+        
+        # Open the file in append mode so that 
+        # All contents of all images are added to the same file
+        f = open(outfile, "a")
+        
+        # Iterate from 1 to total number of pages
+        for i in range(1, filelimit + 1):
+        
+            # Set filename to recognize text from
+            # Again, these files will be:
+            # page_1.jpg
+            # page_2.jpg
+            # ....
+            # page_n.jpg
+            filename = "page_"+str(i)+".jpg"
+                
+            # Recognize the text as string in image using pytesserct
+            text = str(((pytesseract.image_to_string(Image.open(filename)))))
+        
+            # The recognized text is stored in variable text
+            # text = text.replace('-\n', '')    
+
+            text_segs = text.split("\n")
+            # Iterate over every text field
+            for seg_i in range(len(text_segs)):
+                single_text = text_segs[seg_i]
+                for key_phrase in key_phrases:
+                    if key_phrase in single_text:
+                        pprint.pprint([x for x in text_segs[seg_i-2:seg_i+5] if x.strip()])
+                        print("*"*80)
+                        #print(single_text)
+                        ocr_cleanup(f, outfile, files_to_remove)
+                        return
+
+                for regex in key_regexes:
+                    if re.search(regex, single_text):
+                        #print(single_text)
+                        #if "POC" in text_segs[seg_i+1]:
+                        pprint.pprint([x for x in text_segs[seg_i-2:seg_i+5] if x.strip()])
+                        print("*"*80)
+
+                        #print(single_text)
+                        ocr_cleanup(f, outfile, files_to_remove)
+                        return
+
+        # Finally, write the processed text to the file.
+        f.write(text)
+    ocr_cleanup(f, outfile, files_to_remove)
+# ==============================================================================
 def process_ppt(file_name):
     """
     Prints the title of every slide
@@ -82,15 +202,16 @@ def process_ppt(file_name):
         except AttributeError:
             title = slide.shapes[0].text
         print(title)
-
-def process_pdf(file_name):
+# ==============================================================================
+def process_pdf_page_titles(file_name):
     """
     Prints the title of every page (intended for slides in pdf format)
     """    
     text = ''
     with fitz.open(file_name ) as doc:
         for page_count, page in enumerate(doc):
-            #print(f"{page_count}".center(80,"-"))            
+            #print(f"{page_count}".center(80,"-"))   
+            # https://stackoverflow.com/questions/50682486         
             text = page.getText().split('\n')[0]
             print(text)
 
@@ -143,9 +264,10 @@ def main(args):
         if file_extension in ppt_extensions:
             process_ppt(file_name)
         else:
-            #process_pdf(file_name)
+            #process_pdf_page_titles(file_name)
             get_total_budget(file_name)
-            process_pdf_sigs(file_name)
+            if not process_pdf_sigs_fitz(file_name):
+                ocr_pdf(file_name)
 
             # TODO: lowercase and compare to remaining list
     end = time.time()
@@ -162,26 +284,23 @@ if __name__ == "__main__":
 
     # Add an optional argument for the output file,
     # open in 'write' mode and and specify encoding
-    parser.add_argument('--output_dir', 
-                        '-o', 
-                        type=is_directory, 
-                        default=".", 
-                        help="Name of directory to store results.")
-
     parser.add_argument('--file',
                         '-f',
                         action='append', 
-                        type=is_filename)
+                        type=is_filename,
+                        help="Files to parse.  Must be ppt/ppt/pptx")
 
     parser.add_argument('--directory',
                         '-d', 
                         action='append', 
-                        type=is_directory)   
+                        type=is_directory,
+                        help="Directories to search for files to parse")   
 
     parser.add_argument('--keyword',
                         '-k',
                         action='append',
-                        type=str
+                        type=str,
+                        help="Parse only filenames containing ALL of these keywords"
                         )                        
 
     args = parser.parse_args()
